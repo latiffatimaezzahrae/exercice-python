999cada8a0ab1b9b76afd5a2c814442c45a2deb9
latiffatimaezzahrae <latiffatimaezzahrae@gmail.com>
2024-11-03 20:04:31 +0100

    Update exercice3.PY

---
+++ exercice3.PY
@@ -0,0 +1,14 @@
+T1 = [4, 8, 7, 12]
+T2 = [3, 6, 2, -1]
+T = []
+i, j = 0, 0
+while i < len(T1) or j < len(T2):
+    if i < len(T1):
+        T.append(T1[i])
+        i=i+1
+    if j < len(T2):
+        T.append(T2[j])
+        j=j+1
+print(T1)
+print(T2)
+print(T)
